53162daecd5565aea0df891ac2f98fe7961ad47e
metaphor07 <rupak2020.btech@terii.in>
2023-09-05 18:25:33 +0530

    change2

--- DB/connect.js
+++ DB/connect.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // Set the connection timeout to 30 seconds (adjust as needed)
+};
 // Here, is the mongoDB connection define
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL, options)
   .then(() => {
     console.log("Database Connection Successfull...");
   })
